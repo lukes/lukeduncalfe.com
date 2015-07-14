f74fb53ccc2b007562132bf4c58c46c64fbab909
Luke Duncalfe <lduncalfe@eml.cc>
2015-07-14 16:16:06 +1200

    First commit

---
+++ script.js
@@ -0,0 +1,28 @@
+var time = 2000;
+
+var rgba = function() {
+  var a = [];
+  for (var i = 0; i < 3; i++ ) {
+    a.push(Math.round(Math.random() * 255));
+  }
+  return 'rgba(' + a.join(',') + ', .3)';
+};
+
+(function setBodyBackground() {
+  var body = document.getElementsByTagName('body')[0];
+  body.style.backgroundColor = rgba();
+  setTimeout(setBodyBackground, time);
+})();
+
+(function setTitleColor() {
+  var h1 = document.getElementsByTagName('h1')[0];
+  h1.style.color = rgba();
+  setTimeout(setTitleColor, time);
+})();
+
+(function setTitleMargin() {
+  var h1 = document.getElementsByTagName('h1')[0];
+  var v = 0 - Math.round(Math.random() * 7);
+  h1.style.letterSpacing = v + 'vw';
+  setTimeout(setTitleMargin, time);
+})();
